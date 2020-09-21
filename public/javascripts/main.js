357856210023b006fcd5495689714c935bff860c
Gaurav Nehra <gauravnehra07@gmail.com>
2020-09-21 13:25:11 +0530

    Find Colleges for 12th Marks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -35,7 +35,7 @@ function getLists() {
 
 function getCutOff() {
   var course = document.getElementById("course").value;
-  course = course.replace("+", "*plus"); // can't find a beeter solution to encode "+"
+  course = course.replace("+", "*plus"); // can't find a better solution to encode "+"
   var college = document.getElementById("college").value;
   var category = document.getElementById("category").value;
 
@@ -45,7 +45,6 @@ function getCutOff() {
   query = encodeURI(query);
 
   var uri = "http://localhost:3000/cut-off?" + query;
-  console.log(uri);
   var xmlHttpRequest = new XMLHttpRequest();
   xmlHttpRequest.open("GET", uri, true);
   xmlHttpRequest.setRequestHeader("Content-Type", "application/json");
@@ -54,6 +53,42 @@ function getCutOff() {
     if (this.readyState === 4) {
       if (this.status === 200) {
         alert("Your cut off = " + JSON.parse(this.responseText).cutOff);
+      } else {
+        alert(JSON.parse(this.responseText).message);
+        console.log(this.status);
+        console.log(this.responseText);
+      }
+    }
+  };
+  xmlHttpRequest.send();
+}
+
+function getValidColleges() {
+  var course = document.getElementById("course").value;
+  course = course.replace("+", "*plus"); // can't find a better solution to encode "+"
+  var category = document.getElementById("category").value;
+  var marks = document.getElementById("marks").value;
+
+  var query = "course=" + course + "&category=" + category + "&marks=" + marks;
+
+  query = encodeURI(query);
+
+  var uri = "http://localhost:3000/valid-college?" + query;
+  var xmlHttpRequest = new XMLHttpRequest();
+  xmlHttpRequest.open("GET", uri, true);
+  xmlHttpRequest.setRequestHeader("Content-Type", "application/json");
+  xmlHttpRequest.setRequestHeader("Access-Control-Allow-Origin", "*");
+  xmlHttpRequest.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        var colleges = [];
+        colleges = JSON.parse(this.responseText).colleges;
+
+        for (i = 0; i < colleges.length; i++) {
+          var college = document.createElement("li");
+          college.innerText = colleges[i];
+          document.getElementById("valid-colleges").appendChild(college);
+        }
       } else {
         alert(JSON.parse(this.responseText).message);
         console.log(this.status);
