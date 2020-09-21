68cc76bf70b02cecffd52600a480ecfcffa2ac4a
Gaurav Nehra <gauravnehra07@gmail.com>
2020-09-21 11:44:48 +0530

    Dynamic List Generation and Read Original Excel

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,38 @@
+function getLists() {
+  var xmlHttpRequest = new XMLHttpRequest();
+  xmlHttpRequest.open("GET", "http://localhost:3000/list", true);
+  xmlHttpRequest.setRequestHeader("Content-Type", "application/json");
+  xmlHttpRequest.setRequestHeader("Access-Control-Allow-Origin", "*");
+  xmlHttpRequest.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        var colleges = [];
+        var courses = [];
+
+        colleges = JSON.parse(this.responseText).colleges;
+        courses = JSON.parse(this.responseText).courses;
+
+        for (i = 0; i < colleges.length; i++) {
+          var college = document.createElement("option");
+          college.text = colleges[i];
+          college.value = colleges[i];
+          document.getElementById("college").add(college);
+        }
+        for (i = 0; i < courses.length; i++) {
+          var course = document.createElement("option");
+          course.text = courses[i];
+          course.value = courses[i];
+          document.getElementById("course").add(course);
+        }
+      } else {
+        console.log(this.status);
+        console.log(this.responseText);
+      }
+    }
+  };
+  xmlHttpRequest.send();
+}
+
 function getCutOff() {
   var course = document.getElementById("course").value;
   course = course.replace("+", "*plus"); // can't find a beeter solution to encode "+"
@@ -20,6 +55,7 @@ function getCutOff() {
       if (this.status === 200) {
         alert("Your cut off = " + JSON.parse(this.responseText).cutOff);
       } else {
+        alert(JSON.parse(this.responseText).message);
         console.log(this.status);
         console.log(this.responseText);
       }
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,29 @@ var express = require("express");
 var router = express.Router();
 const pdf2excel = require("pdf-to-excel");
 const xlsx = require("xlsx");
+const nodexlsx = require("node-xlsx");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "DU Cut Off Calculator" });
+});
+
+router.get("/list", function (req, res, next) {
+  var obj = nodexlsx.parse("./public/files/ba-cut-off.xlsx");
+
+  var colleges = [];
+  var courses = [];
+
+  colleges = obj[0].data[0];
+  colleges.shift();
+
+  for (i = 1; i < obj[0].data.length; i++) {
+    courses.push(obj[0].data[i][0]);
+  }
+
+  res
+    .status(200)
+    .send({ message: "Success", colleges: colleges, courses: courses });
 });
 
 router.get("/cut-off", function (req, res, next) {
@@ -13,25 +32,37 @@ router.get("/cut-off", function (req, res, next) {
   var college = req.query.college;
   var category = req.query.category;
 
-  var workbook = xlsx.readFile("./public/files/cut-off.xlsx");
+  var workbook = xlsx.readFile("./public/files/ba-cut-off.xlsx");
   var sheet_name_list = workbook.SheetNames;
   var xlData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
   var cutOff = "";
   for (i = 0; i < xlData.length; i++) {
-    if (xlData[i].__EMPTY === course) {
+    if (xlData[i]["Name of Course"] === course) {
       cutOff = xlData[i][college];
       break;
     }
   }
 
-  var cutOffArray = cutOff.split("\n");
+  if (typeof cutOff === "undefined") {
+    res.status(404).send({ message: "Not Found" });
+  } else {
+    cutOff = cutOff.replace(/([a-zA-Z])\w|\s|\t|\r/gm, "");
 
-  res.status(200).send({ message: "success", cutOff: cutOffArray[category] });
+    var cutOffArray = cutOff.split("\n");
+
+    if (cutOffArray[category] == "") {
+      res.status(200).send({ message: "success", cutOff: "Closed or NA" });
+    } else {
+      res
+        .status(200)
+        .send({ message: "success", cutOff: cutOffArray[category] });
+    }
+  }
 });
 
 // using node-xlsx
-// var obj = nodexlsx.parse("./public/files/cut-off.xlsx");
+// var obj = nodexlsx.parse("./public/files/ba-cut-off.xlsx");
 // console.log(obj[0].data[1]);
 
 module.exports = router;
