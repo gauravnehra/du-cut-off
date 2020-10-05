6e04911dd31988d0728ab16541322ce88fea266b
Gaurav Nehra <gauravnehra07@gmail.com>
2020-10-05 15:09:12 +0530

    Install cors package

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 var express = require("express");
 var path = require("path");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 
 var app = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use("/", indexRouter);
 
